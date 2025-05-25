d7539fde1533c3f0c8b56512afc7893918c3fdbb
ibra13-13 <ibra132001@gmail.com>
2025-05-25 18:50:51 +0000

    new files

---
+++ class4/variables.tf
@@ -0,0 +1,14 @@
+variable aws_key {
+    default  = "hello-key"
+    type = string
+}
+
+variable region {
+    default = "us-east-2"
+    type = string
+}
+
+variable port {
+    default = [22, 80]
+    type = list
+}
